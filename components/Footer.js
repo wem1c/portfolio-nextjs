9fbcfc14769916e275aa267cd8b38e5a4dd192dc
Conor C. Peterson <conorpetersondev@gmail.com>
2022-09-15 15:52:06 +0200

    Clean up footer code.
    Reformat code, remove unnecessary code, remove Twitter link.

--- components/Footer.js
+++ components/Footer.js
@@ -6,26 +6,24 @@ export default function Footer() {
     <div className="bg-[#F1F1F1] dark:bg-gray-900">
       <div className="max-w-6xl  mx-auto px-4 py-10 md:py-20">
         <div className="flex flex-col space-y-4 md:space-y-0 md:flex-row justify-between md:items-center mt-8">
-          <div>
-            <p>&copy; Conor C. Peterson. All Rights Reserved.</p>
+          {/** Copyright */}
+          <p>&copy; Conor C. Peterson. All Rights Reserved.</p>
+          {/** Template credit */}
+          <div className="flex">
+            <p>Based on a template by</p>
+            <div className="inline-block transform hover:scale-110 hover:-rotate-3 transition duration-300">
+              <a
+                className="hover:bg-red-500 rounded-md px-2 py-1  hover:text-gray-50"
+                href="https://manuarora.in"
+              >
+                Manu Arora
+              </a>
+            </div>
           </div>
-          <div>
-            {/* Support me by keeping this in the footer, please. :) */}
-            <p>
-              Developed by
-              <div className="inline-block transform hover:scale-110 hover:-rotate-3 transition duration-300">
-                <a
-                  className="hover:bg-red-500 rounded-md px-2 py-1  hover:text-gray-50"
-                  href="https://manuarora.in"
-                >
-                  Manu Arora
-                </a>
-              </div>
-            </p>
-          </div>
+          {/** Social media links */}
           <div className="space-x-4 flex flex-row items-center">
             <ul className="flex">
-              <li className="mr-2">
+              <li className="mr-2" key={"ig-footer"}>
                 <a
                   href={userData.socialLinks.instagram}
                   className="text-base font-normal text-gray-600 dark:text-gray-300"
@@ -42,24 +40,7 @@ export default function Footer() {
                   </svg>
                 </a>
               </li>
-              <li className="mr-2">
-                <a
-                  href={userData.socialLinks.twitter}
-                  className="text-base font-normal text-gray-600 dark:text-gray-300"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    className="bi bi-twitter h-5 w-5"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
-                  </svg>
-                </a>
-              </li>
-              <li>
+              <li key={"linkedin-footer"}>
                 <a
                   href={userData.socialLinks.linkedin}
                   className="text-base font-normal text-gray-600 dark:text-gray-300"
