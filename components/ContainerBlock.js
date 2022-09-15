ca626e63e897e5ca02ec7e86e555c3d3c6c04c6f
Conor C. Peterson <conorpetersondev@gmail.com>
2022-09-15 15:16:58 +0200

    Clean up metadata and general code.
    Remove Twitter metadata, update personal data values, add comments.

--- components/ContainerBlock.js
+++ components/ContainerBlock.js
@@ -4,49 +4,53 @@ import { useRouter } from "next/router";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
+/**
+ * A wrapper component for the Head, Navbar, and Footer components shared by every page.
+ *
+ * @param {object} children The inner components of the ContainerBlock component passed as a parameter by Next.
+ * @param {object} customMeta Custom meta properties for the Head component.
+ * @returns Page with Head, Navbar, main, Footer.
+ */
 export default function ContainerBlock({ children, ...customMeta }) {
   const router = useRouter();
 
   const meta = {
-    title: "Conor C. Peterson - Developer, Writer, Creator and YouTuber",
-    description: `I've been developing websites for 5 years straight. Get in touch with me to know more.`,
+    title: "Conor C. Peterson",
+    description: `Full-Stack Web Developer. Have an idea you want to make happen? Contact me and let's bring it to life!`,
     image: "/avatar.png",
+    "image-alt": "avatar image",
     type: "website",
     ...customMeta,
   };
+
   return (
     <>
       <Head>
         <title>{meta.title}</title>
+        <meta name="description" content={meta.description} />
         <meta name="robots" content="follow, index" />
-        <meta content={meta.description} name="description" />
-        <meta
-          property="og:url"
-          content={`https://yourwebsite.com${router.asPath}`}
-        />
         <link
           rel="canonical"
-          href={`https://yourwebsite.com${router.asPath}`}
+          href={`https://conorpeterson.com${router.asPath}`}
+        />
+        {/* OpenGraph metadata */}
+        <meta
+          property="og:url"
+          content={`https://conorpeterson.com${router.asPath}`}
         />
         <meta property="og:type" content={meta.type} />
-        <meta property="og:site_name" content="Manu Arora" />
+        <meta property="og:site_name" content="Conor C. Peterson" />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@mannupaaji" />
-        <meta name="twitter:title" content={meta.title} />
-        <meta name="twitter:description" content={meta.description} />
-        <meta name="twitter:image" content={meta.image} />
+        <meta property="og:image:alt" content={meta.image} />
         {meta.date && (
           <meta property="article:published_time" content={meta.date} />
         )}
       </Head>
       <Navbar />
-      <main className="w-full">
-        <>{children}</>
-        <Footer />
-      </main>
+      <main className="w-full">{children}</main>
+      <Footer />
     </>
   );
 }
