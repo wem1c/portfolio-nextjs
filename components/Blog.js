fe9cd0f1cd6723a83ae449f1305e9c3fb61c72f4
Conor C. Peterson <conorpetersondev@gmail.com>
2022-11-26 16:04:23 +0100

    Remove dark mode references.

--- components/Blog.js
+++ components/Blog.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Blog() {
   return (
-    <section className="bg-white dark:bg-gray-800">
-      <div className="max-w-6xl mx-auto h-48 bg-white dark:bg-gray-800">
+    <section className="bg-white">
+      <div className="max-w-6xl mx-auto h-48 bg-white">
         <h1 className=" text-5xl md:text-9xl font-bold py-20 text-center md:text-left">
           Blog
         </h1>
