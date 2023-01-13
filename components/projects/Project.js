5477411ef9b430fb3fcfa9dbd1b5ae0ea2a05f2e
Conor C. Peterson <conorpetersondev@gmail.com>
2023-01-13 18:22:58 +0100

    feat: add animation to component
    Add scaling animation to the project image when hovered

--- components/projects/Project.js
+++ components/projects/Project.js
@@ -5,7 +5,11 @@ const Project = ({ link, image, title }) => {
   return (
     <Link href={link}>
       <div className="relative overflow-hidden shadow-lg">
-        <Image src={image} alt="project image" />
+        <Image
+          src={image}
+          alt="project image"
+          className="transition-transform ease-in-out duration-700 hover:scale-150"
+        />
         <h1 className="absolute top-10 left-5 right-5 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2 w-fit">
           {title}
         </h1>
