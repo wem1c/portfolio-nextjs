b8b1c0de2f0c4b85c12ce9c99b7e8ef67e31d1c4
Conor C. Peterson <conorpetersondev@gmail.com>
2023-02-25 18:02:36 +0100

    refactor: update styling and add SocialMediaLink
    Update old styling, add new hero image, add hero title content

--- components/Hero.js
+++ components/Hero.js
@@ -6,16 +6,17 @@ import Image from "next/image";
 import RainbowHighlight from "./RainbowHighlight";
 
 // Data imports
-import avatar from "../public/avatar.png";
+import homeRunSVG from "@public/images/home_run.svg";
 
 export default function Hero() {
   const highlightedWords = [
     { text: "Developer", color: "#FFAD54" },
-    { text: "Programmer", color: "#AD85FF" },
-    { text: "Engineer", color: "#10B32A" },
+    { text: "Programmer", color: "#3BCBFF" },
+    { text: "Engineer", color: "#B8FF6E" },
+    { text: "Researcher", color: "#E847FF" },
   ];
   return (
-    <section className='bg-green-800 py-5'>
+    <section className='p-5'>
       {/* Rough text container */}
       <div className='max-w-7xl mx-auto flex flex-col items-center overflow-hidden md:flex-row'>
         <div className='w-full mx-auto px-5 text-center'>
@@ -23,7 +24,7 @@ export default function Hero() {
             {highlightedWords.map((word, _idx) => {
               return (
                 <RainbowHighlight key={_idx} color={word.color}>
-                  <h1 className='text-5xl font-bold text-green-800 my-2'>
+                  <h1 className='text-4xl md:text-5xl font-bold text-green-800 my-2'>
                     {word.text}
                   </h1>
                 </RainbowHighlight>
@@ -31,8 +32,13 @@ export default function Hero() {
             })}
           </RoughNotationGroup>
         </div>
-        {/* Image container */}
-        <Image src={avatar} alt='avatar' className='flex w-1/2' />
+        <Image
+          priority
+          src={homeRunSVG}
+          quality={100}
+          alt='image of a man batting a baseball'
+          className='flex w-2/3 md:w-1/2 lg:w-1/3'
+        />
       </div>
     </section>
   );
