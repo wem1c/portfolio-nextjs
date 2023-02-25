39b8a15fc1a1209f71db1373e13feb370d44d4c6
Conor C. Peterson <conorpetersondev@gmail.com>
2023-02-25 18:11:55 +0100

    style: update styling
    Update styling to reflect the rest of the components.

--- components/Repository.js
+++ components/Repository.js
@@ -3,9 +3,9 @@ import Link from "next/link";
 
 const Repository = ({ repo }) => {
   return (
-    <Link href={repo.clone_url}>
-      <h1 className='font-semibold text-xl text-tan'>{repo.name}</h1>
-      <p className='text-base font-normal text-gray-200'>{repo.description}</p>
+    <Link href={repo.clone_url} className='text-center'>
+      <h1 className='font-semibold text-xl'>{repo.name}</h1>
+      <p className='text-base font-normal text-gray-500'>{repo.description}</p>
     </Link>
   );
 };
