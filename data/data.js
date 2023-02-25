48ce4b66c78eb80b1cb55ff61b7cdb0e5c29bb44
Conor C. Peterson <conorpetersondev@gmail.com>
2023-02-25 17:58:48 +0100

    refactor: rename local data file
    Rename "constants.js" to the more reasonable "data.js"

---
+++ data/data.js
@@ -0,0 +1,26 @@
+const userData = {
+  projects: [
+    {
+      title: "Tailwind Master Kit",
+      link: "https://tailwindmasterkit.com",
+      imgUrl: "/tmk.jpg",
+    },
+  ],
+  about: {
+    description: [
+      "Hello there! My name is Conor and this is my humble abode on the internet.",
+
+      "As you may have gathered already, I am a Fullstack Web Developer. What this means is that I create websites/web-apps all the way through. From the code that makes the looks to the server APIs that manage data, all the way to the databases that store it! (and everything else in-between.)",
+
+      "UI/UX intuitiveness, performance, and scalability are what I focus on, but I don't skimp on style! I love exploring new tech, so for these projects I always try to pick the most appropriate tech. (I do love me some Jamstack, though!)",
+
+      "Anything computing-related is interesting to me, so I do wear many more hats in the CS/IT space. I use Linux, I'm learning Android and Cloud Development, and I research AI and Machine Learning.",
+
+      "In my free time I read a lot of research papers, books, articles, and I write about what I find interesting. You may catch some of my writing over on the blog!",
+
+      "Some of my pastimes also include, but are not limited to, video games, TTRPGs, YouTube, and Twitch. I also listen to a lot of music. I'm not joking, my Spotify stats are booming.",
+    ],
+  },
+};
+
+export default userData;
