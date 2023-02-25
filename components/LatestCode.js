efb4c87894c083e61fe69808bea650828dbe9a2a
Conor C. Peterson <conorpetersondev@gmail.com>
2023-02-25 18:09:21 +0100

    style: refactor styling
    Update styling to reflect the rest of the components.

--- components/LatestCode.js
+++ components/LatestCode.js
@@ -8,17 +8,18 @@ import userData from "data/data";
 
 export default function LatestCode({ repositories }) {
   return (
-    <section className='bg-green-800 space-y-5 py-5'>
+    <section className='space-y-10 p-5'>
       <header className='flex flex-col items-center space-y-5'>
-        <h1 className='text-6xl font-bold text-tan text-center'>Latest Code</h1>
-
+        <h1 className='text-4xl text-green-800 font-bold text-center'>
+          Latest Repos
+        </h1>
         <Button
           url={`https://github.com/${userData.githubUsername}`}
-          text={"View GitHub"}
+          text={"View All"}
         />
       </header>
 
-      <ul className='grid gap-10 max-w-7xl mx-auto px-10 md:grid-cols-2'>
+      <ul className='grid gap-10 max-w-7xl mx-auto px-10 md:grid-cols-2 lg:grid-cols-3'>
         {repositories.length > 0 ? (
           repositories.map((repository, _idx) => {
             return <Repository key={_idx} repo={repository} />;
