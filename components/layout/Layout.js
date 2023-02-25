2f9d2b4225dae9f8fc93f65c4a9e6f969320f56b
Conor C. Peterson <conorpetersondev@gmail.com>
2023-02-25 18:11:20 +0100

    refactor: move social media link data
    Move social media link data to Layout component and pass to Footer and Navbar components through props.

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -2,13 +2,37 @@ import Meta from "@components/Meta";
 import Navbar from "./nav/Navbar";
 import Footer from "./Footer";
 
+import {
+  faMastodon,
+  faLinkedin,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
+
+const fontAwesomeIcons = [
+  {
+    href: "https://social.linux.pizza/@wemic",
+    icon: faMastodon,
+    alt: "Mastodon icon",
+  },
+  {
+    href: "https://www.linkedin.com/in/conor-peterson/",
+    icon: faLinkedin,
+    alt: "LinkedIn icon",
+  },
+  {
+    href: "https://github.com/wem1c",
+    icon: faGithub,
+    alt: "GitHub icon",
+  },
+];
+
 export default function Layout({ children }) {
   return (
     <>
       <Meta />
-      <Navbar />
+      <Navbar icons={fontAwesomeIcons} />
       <main>{children[0]}</main>
-      <Footer />
+      <Footer icons={fontAwesomeIcons} />
     </>
   );
 }
