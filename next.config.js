957b030807380d2fdf9cb3304509a6ca063fd5b7
Conor C. Peterson <conorpetersondev@gmail.com>
2023-02-26 22:46:27 +0100

    lighthouse: optimize image sizes
    Define image sizes in next.config.js and then implement responsive sizes for next/image components

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
-module.exports = withBundleAnalyzer({});
+module.exports = withBundleAnalyzer({
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+  productionBrowserSourceMaps: true,
+  images: {
+    deviceSizes: [640, 768, 1024, 1280, 1536],
+  },
+});
