16d15abb3d94827f2c8dfb2708f14dfe9604c3fd
Conor C. Peterson <conorpetersondev@gmail.com>
2023-02-26 16:58:41 +0100

    refactor: remove unused code
    Remove unnecessary imports and config declarations for FontAwesome

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,4 @@
-// Modules
-import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
-config.autoAddCss = false;
-
-// Components
 import Layout from "@components/layout/Layout";
 import "../styles/globals.css";
 
