838570bd3a1924e1ee2136fe0398ad29d8c6ba2e
Conor C. Peterson <conorpetersondev@gmail.com>
2023-02-26 16:57:44 +0100

    refactor: implement new components
    Rewrite the code to use new project display related components.

--- components/projects/FavouriteProjects.js
+++ components/projects/FavouriteProjects.js
@@ -1,15 +1,27 @@
 // Component imports
 import Button from "@components/Button";
-import Project from "./Project";
+import ProjectGrid from "./ProjectGrid";
 
 // Image imports
+import portfolioImage from "@public/images/projects/portofolio_1600x900.png";
 import pokemonImage from "@public/images/projects/pokemon_1600x900.png";
+import yourProjectImage from "@public/images/projects/your_project.png";
 
 const favoriteProjects = [
   {
+    link: "https://conorpeterson.com/",
+    image: portfolioImage,
+    title: "This website!",
+  },
+  {
     link: "https://pokemon-type-chart.vercel.app/",
     image: pokemonImage,
-    title: "Responsive Pokemon Type Chart",
+    title: "Responsive Pokemon Chart",
+  },
+  {
+    link: "",
+    image: yourProjectImage,
+    title: "Your project here!",
   },
 ];
 
@@ -17,29 +29,13 @@ export default function FavouriteProjects() {
   return (
     <section className='bg-green-800 space-y-10 p-5'>
       <header className='flex flex-col items-center space-y-5'>
-        <h1 className='text-4xl text-tan font-bold text-center'>
+        <h1 className='text-3xl md:text-4xl text-tan font-bold text-center'>
           Favorite Projects
         </h1>
         <Button url={"/projects"} text={"View All"} />
       </header>
 
-      {/* Grid starts here */}
-      <div className='grid justify-center max-w-7xl mx-auto'>
-        {favoriteProjects.length > 0 ? (
-          favoriteProjects.map((project, _idx) => {
-            return (
-              <Project
-                key={_idx}
-                link={project.link}
-                image={project.image}
-                title={project.title}
-              />
-            );
-          })
-        ) : (
-          <p>No projects found.</p>
-        )}
-      </div>
+      <ProjectGrid projects={favoriteProjects} />
     </section>
   );
 }
