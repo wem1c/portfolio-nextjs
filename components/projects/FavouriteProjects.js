ad275ccff91d41785d1b71e322d4edd080bc2915
Conor C. Peterson <conorpetersondev@gmail.com>
2023-02-27 17:34:46 +0100

    style: re-order project listing
    Make Portfolio website project appear later in the list.

--- components/projects/FavouriteProjects.js
+++ components/projects/FavouriteProjects.js
@@ -9,14 +9,14 @@ import yourProjectImage from "@public/images/projects/your_project.png";
 
 const favoriteProjects = [
   {
-    link: "https://conorpeterson.com/",
-    image: portfolioImage,
-    title: "This website!",
-  },
-  {
     link: "https://pokemon-type-chart.vercel.app/",
     image: pokemonImage,
     title: "Responsive Pokemon Chart",
+  },
+  {
+    link: "https://conorpeterson.com/",
+    image: portfolioImage,
+    title: "This website!",
   },
   {
     link: "",
