f93f0314fdc1aa4e61b247338d324b2d32db5e13
Conor C. Peterson <conorpetersondev@gmail.com>
2023-02-27 18:58:00 +0100

    fix: update link href
    Fix GH link href in component to reflect actual username. Remove unnecessary import.

--- components/LatestCode.js
+++ components/LatestCode.js
@@ -1,7 +1,6 @@
 // Component imports
 import Button from "./Button";
 import Repository from "./Repository";
-import userData from "data/data";
 
 export default function LatestCode({ repositories }) {
   return (
@@ -10,10 +9,7 @@ export default function LatestCode({ repositories }) {
         <h1 className='text-3xl md:text-4xl text-green-800 font-bold text-center'>
           Latest Repos
         </h1>
-        <Button
-          url={`https://github.com/${userData.githubUsername}`}
-          text={"View All"}
-        />
+        <Button url={`https://github.com/wem1c`} text={"View All"} />
       </header>
 
       <div className='grid gap-10 max-w-7xl mx-auto px-10 md:grid-cols-2 lg:grid-cols-3'>
