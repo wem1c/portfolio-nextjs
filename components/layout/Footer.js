5b899588be5afc71c8499cc699327021d177fd09
Conor C. Peterson <conorpetersondev@gmail.com>
2023-02-27 15:34:57 +0100

    refactor: optimize social icons
    Increase the size of clickable social icons to at least 48x48, and remove them from the navbar alltogether

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -5,12 +5,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function Footer({ icons }) {
   return (
     <div className='p-5 mt-5 flex flex-col space-y-5 justify-between items-center'>
-      <p className='text-lg'>Made with ❤️ by Conor C. Peterson</p>
+      <p className='text-md md:text-lg text-center'>
+        Made with ❤️ by Conor C. Peterson
+      </p>
       <div className='space-x-5 flex flex-row items-center'>
         {icons.map((icon, idx) => {
           return (
             <Link key={idx} href={icon.href}>
-              <FontAwesomeIcon icon={icon.icon} size='xl' />
+              <FontAwesomeIcon icon={icon.icon} size='3x' />
               <span className='sr-only'>{icon.alt}</span>
             </Link>
           );
--- components/layout/nav/Navbar.js
+++ components/layout/nav/Navbar.js
@@ -1,6 +1,5 @@
 // Module imports
 import Link from "next/link";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 // Component imports
 import NavLink from "./NavLink";
@@ -12,36 +11,22 @@ const pages = [
   { name: "Blog", path: "/blog" },
 ];
 
-export default function Navbar({ icons }) {
+export default function Navbar() {
   return (
-    <div className='max-w-7xl mx-auto p-5'>
-      <div className='flex md:flex-row justify-between items-center'>
-        <div className='flex flex-col'>
-          <Link href='/'>
-            <h1 className='font-semibold text-green-800 text-2xl'>
-              Conor C. Peterson
-            </h1>
-            <p className='text-base font-light text-gray-600'>
-              Fullstack Developer
-            </p>
-          </Link>
-        </div>
-        <nav className='hidden md:flex space-x-5'>
-          {pages.map((page, idx) => (
-            <NavLink key={idx} name={page.name} path={page.path} />
-          ))}
-        </nav>
-        <div className='flex space-x-6 items-center'>
-          {icons.map((icon, idx) => {
-            return (
-              <Link key={idx} href={icon.href}>
-                <FontAwesomeIcon icon={icon.icon} size='xl' />
-                <span className='sr-only'>{icon.alt}</span>
-              </Link>
-            );
-          })}
-        </div>
-      </div>
+    <div className='max-w-7xl mx-auto p-5 flex flex-col md:flex-row justify-between items-center'>
+      <Link href='/' className='flex flex-col text-center md:text-left'>
+        <h1 className='font-semibold text-green-800 text-2xl'>
+          Conor C. Peterson
+        </h1>
+        <p className='text-base font-light text-gray-600'>
+          Fullstack Developer
+        </p>
+      </Link>
+      <nav className='hidden md:flex space-x-5'>
+        {pages.map((page, idx) => (
+          <NavLink key={idx} name={page.name} path={page.path} />
+        ))}
+      </nav>
 
       <nav className='flex md:hidden mt-4 justify-center space-x-5'>
         {pages.map((page, idx) => (
