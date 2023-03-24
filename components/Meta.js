bce7413bb2b490424054ca721371a40fca68f450
Conor C. Peterson <conorpetersondev@gmail.com>
2023-03-24 14:45:23 +0100

    refactor: replace favicons
    Replace old favicons with new ones. This includes:
    - adding a new 'favicons' sub-folder to 'public'
    - adding new favicon files to 'favicons'
    - deleting old favicon files from 'public'
    - updating the href attributes in meta tags to reflect new favicon location

--- components/Meta.js
+++ components/Meta.js
@@ -30,32 +30,32 @@ export default function Meta() {
       <link
         rel='apple-touch-icon'
         sizes='180x180'
-        href='/apple-touch-icon.png'
+        href='/favicons/apple-touch-icon.png'
       />
       <link
         rel='icon'
         type='image/png'
         sizes='32x32'
-        href='/favicon-32x32.png'
+        href='/favicons/favicon-32x32.png'
       />
       <link
         rel='icon'
         type='image/png'
         sizes='16x16'
-        href='/favicon-16x16.png'
+        href='/favicons/favicon-16x16.png'
       />
-      <link rel='icon' href='/favicon.ico' />
+      <link rel='icon' href='/favicons/favicon.ico' />
       <link
         rel='shortcut icon'
         sizes='192x192'
-        href='/android-chrome-192x192.png'
+        href='/favicons/android-chrome-192x192.png'
       />
       <link
         rel='shortcut icon'
         sizes='512x512'
-        href='/android-chrome-512x512.png'
+        href='/favicons/android-chrome-512x512.png'
       />
-      <link rel='manifest' href='/site.webmanifest' />
+      <link rel='manifest' href='/favicons/site.webmanifest' />
       <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
       <meta name='msapplication-TileColor' content='#da532c' />
       <meta name='theme-color' content='#ffffff' />
