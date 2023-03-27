74385924e643e1423949f8041d979a7566841178
Conor C. Peterson <conorpetersondev@gmail.com>
2023-03-28 00:52:21 +0200

    fix: repair postcss config file
    Remove recently added postcss plugin packages, and add cssnano package.

--- postcss.config.js
+++ postcss.config.js
@@ -1,25 +1,7 @@
 module.exports = {
   plugins: {
     tailwindcss: {},
-    ...(process.env.NODE_ENV === "production"
-      ? [
-          "postcss-flexbugs-fixes",
-          [
-            "postcss-preset-env",
-            {
-              autoprefixer: {
-                flexbox: "no-2009",
-              },
-              cssnano: {},
-              // stage: 3,
-              features: {
-                "custom-properties": false,
-              },
-            },
-          ],
-        ]
-      : [
-          // No transformations in development
-        ]),
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {}),
   },
 };
