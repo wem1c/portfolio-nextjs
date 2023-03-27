11e441d9d339de32af19cae8fcd37a511d8d95dc
Conor C. Peterson <conorpetersondev@gmail.com>
2023-03-27 23:09:30 +0200

    refactor: add new component
    Add a new component wrapper for a grid of items, and edit old code to use the new component.
    
    Change:
    - replace custom ProjectGrid component with new ItemGrid component
    - replace custom repo grid code with new ItemGrid component
    - remove unnecessary Project component, and move its code to the projects page

--- pages/projects.js
+++ pages/projects.js
@@ -1,5 +1,29 @@
 import Head from "next/head";
-import Projects from "@components/Projects";
+
+import ItemGrid from "@components/ItemGrid";
+import ProjectCard from "@components/projects/ProjectCard";
+
+import portfolioImage from "@public/images/projects/portofolio_1600x900.png";
+import pokemonImage from "@public/images/projects/pokemon_1600x900.png";
+import yourProjectImage from "@public/images/projects/your_project.png";
+
+const allProjects = [
+  {
+    link: "https://conorpeterson.com/",
+    image: portfolioImage,
+    title: "This website!",
+  },
+  {
+    link: "https://pokemon-type-chart.vercel.app/",
+    image: pokemonImage,
+    title: "Responsive Pokemon Chart",
+  },
+  {
+    link: "",
+    image: yourProjectImage,
+    title: "Your project here!",
+  },
+];
 
 export default function projects() {
   return (
@@ -7,7 +31,26 @@ export default function projects() {
       <Head>
         <title>Projects - Conor C. Peterson</title>
       </Head>
-      <Projects />
+
+      <section className='space-y-10 p-5'>
+        <ItemGrid>
+          {allProjects.length > 0 ? (
+            allProjects.map((project, _idx) => {
+              return (
+                <ProjectCard
+                  key={_idx}
+                  link={project.link}
+                  image={project.image}
+                  title={project.title}
+                  priority={"true"}
+                />
+              );
+            })
+          ) : (
+            <p>No projects.</p>
+          )}
+        </ItemGrid>
+      </section>
     </>
   );
 }
