8ea10c604334a26ef436cfaf2b1af579dc105e27
Conor C. Peterson <conorpetersondev@gmail.com>
2023-03-27 23:07:06 +0200

    refactor: update PostCSS config
    Add default Next.js PostCSS config because it is ommited when a custom postcss.config.js file is present!

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,25 @@
 module.exports = {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {},
+    ...(process.env.NODE_ENV === "production"
+      ? [
+          "postcss-flexbugs-fixes",
+          [
+            "postcss-preset-env",
+            {
+              autoprefixer: {
+                flexbox: "no-2009",
+              },
+              cssnano: {},
+              // stage: 3,
+              features: {
+                "custom-properties": false,
+              },
+            },
+          ],
+        ]
+      : [
+          // No transformations in development
+        ]),
   },
-}
+};
